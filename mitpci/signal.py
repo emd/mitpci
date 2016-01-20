14db16ae3af05aaaee052714d71e479574b0e331
emd <emd@mit.edu>
2016-01-20 15:33:38 -0800

    Added determination of digitizer board and tests

--- mitpci/signal.py
+++ mitpci/signal.py
@@ -14,5 +14,33 @@ import MDSplus as mds
 
 
 class Signal(object):
-    def __init__(self, shot, channel=None, Fs=None, tlim=None):
+    def __init__(self, shot, channel, Fs=None, tlim=None):
+        self.shot = shot
+        self.channel = channel
+
+        self.digitizer_board = self.getDigitizerBoard()
+
+        # mds_tree = mds.Tree('pci', shot=shot, mode='ReadOnly')
+        pass
+
+    def getDigitizerBoard(self, channels_per_board=8):
+        'Get digitizer board corresponding to `self.channel`.'
+        if (self.channel >= 1) and (self.channel <= channels_per_board):
+            return 'DT216_7'
+        elif self.channel <= (2 * channels_per_board):
+            return 'DT216_8'
+        else:
+            raise ValueError(
+                'Valid `channel` values between 1 and %i' %
+                (2 * channels_per_board))
+
+    def getSampleRate(self, mds_tree):
+        # need digitizer...
+        # Fs = mds_tree.getNode(self._digitizers[0] + ':CLOCK_DIV').data()
+        pass
+
+    def getInitialTime(self):
+        pass
+
+    def getSignal(self):
         pass
