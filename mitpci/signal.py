7961d75af3ee17f7cfecfe68c8dcd8e9b01008b4
emd <emd@mit.edu>
2016-01-21 18:09:52 -0800

    Call `_getSignal()` during __init__()

--- mitpci/signal.py
+++ mitpci/signal.py
@@ -32,7 +32,7 @@ class Signal(object):
         # time window and with the specified sampling rate
         mds_tree = mds.Tree('pci', shot=shot, mode='ReadOnly')
         self.Fs, self._downsample = self._getSampleRate(mds_tree, Fs=Fs)
-        # self.t0, self.x = self._getSignal(mds_tree, tlim=tlim)
+        self.t0, self.x = self._getSignal(mds_tree, tlim=tlim)
 
     def _checkChannels(self, channels_per_board=8):
         'Ensure that `self.channel` corresponds to a physical mitpci channel.'
