36856625899674ed86be12eb02255d5232b55ed7
emd <emd@mit.edu>
2016-01-22 18:23:02 -0800

    Added a ton of documentation

--- mitpci/signal.py
+++ mitpci/signal.py
@@ -1,10 +1,5 @@
-'''This module implements a class for interacting with the signals
-digitized by the `mitpci` computer system.
-
-The `mitpci` computer system digitizes data from MIT's (a) phase contrast
-imaging (PCI) and (b) heterodyne interferometer systems. The systems share
-a beam path through the DIII-D vessel and a digitizer, but they use different
-interference schemes and detectors to make their respective measurements.
+'''This module implements a class for retrieving signals digitized by
+the mitpci system.
 
 '''
 
@@ -14,8 +9,116 @@ import MDSplus as mds
 
 
 class Signal(object):
+    '''An object corresponding to the signal retrieved from the mitpci system.
+
+    Attributes:
+    -----------
+    shot - int
+        The shot number of the retrieved signal.
+
+    channel - int
+        The channel number of the retrieved signal.
+
+    x - array-like, (`N`,)
+        The retrieved signal. Note that this is the "raw" signal and
+        has the units of bits.
+        [x] = bits
+
+    Fs - float
+        The sampling rate at which the signal was retrieved. Note that this
+        may be smaller than the signal's initial digitization rate; that is,
+        the retrieved signal may be downsampled relative to the full signal.
+        [Fs] = samples / second
+
+    t0 - float
+        The time corresponding to the first retrieved point in the signal;
+        that is, if x(t) corresponds to the continuous signal being sampled,
+        then `Signal.x[0]` = x(t0)
+        [t0] = s
+
+    t - array-like, (`N`,)
+        The times corresponding to the points in the retrieved signal.
+        This attribute is actually implemented as a class property
+        so that the time-base is generated on the fly as needed and
+        is not stored as an object property; this helps save memory and
+        processing time.
+        [t] = s
+
+    '''
     def __init__(self, shot, channel, channels_per_board=8,
                  Fs=None, tlim=None):
+        '''Create an instance of the `Signal` class.
+
+        Input parameters:
+        -----------------
+        shot - int
+            The shot number of the signal to be retrieved.
+
+        channel - int
+            The channel number of the signal to be retrieved.
+            Currently, there are two digitizer boards, each of which
+            digitizes `channels_per_board` channels. A ValueError is
+            raised if `channel <= 0` or `channel > 2 * channels_per_board`.
+
+        channels_per_board - int
+           The number of channels digitized per digitizer board.
+           Currently, there are two digitizer boards, each of which
+           digitizes 8 channels. The value of `channels_per_board`
+           determines whether the value of `channel` is valid or not.
+
+        Fs - float
+            The desired sampling rate at which to retrieve the signal.
+            This may differ from the original digitization rate, `Fs_dig`;
+            that is, the retrieved signal can be downsampled. While
+            the specified `Fs` can be any positive value, the internal
+            methods of this class will process the user's request and
+            produce a *realized* sampling rate `Signal.Fs` that is
+            subject to the following two constraints:
+
+                (1) `Signal.Fs <= Fs_dig`, and
+                (2) `Signal. Fs / Fs_dig = m`, where `m` is an integer
+
+            `Fs <= 0` raises a ValueError.
+
+            Note: the "raw" signal is returned from the MDSplus server.
+            Josh Stillerman and Tom Fredian (of MIT, and the developers
+            of MDSplus) informed me that there is no way to read in only
+            a portion of the raw data. Thus, the entire raw signal must be
+            read into memory, and the signal will subsequently be sliced
+            in time if needed. As a result, specifying a small `Fs` will
+            not save computational time but will end up saving memory.
+
+            [Fs] = samples / second
+
+        tlim - array-like, (2,)
+            The lower and upper limits in time for which the signal
+            will be retrieved.
+
+            The specified `tlim` values will always bracket the retrieved
+            signal. That is, if `tlim[0]` does not correspond to an exact
+            digitization time, then the initial time returned (`Signal.t0`)
+            will be the closest digitization time *greater* than `tlim[0]`.
+            Similarly, if `tlim[1]` does not correspond to an exact
+            digitization time, then the final time (`Signal.t[-1]`) will be
+            the closest digitization time *less* than `tlim[1]`. Further,
+            if `tlim[0]` is less than the initial digitization time,
+            the retrieved signal will begin with the initial digitized point.
+            Similarly, if `tlim[1]` exceeds the final digitization time,
+            the retrieved signal will end with the final digitized point.
+
+            A ValueError is raised if `len(tlim) != 2`.
+
+            Note: the "raw" signal is returned from the MDSplus server.
+            Josh Stillerman and Tom Fredian (of MIT, and the developers
+            of MDSplus) informed me that there is no way to read in only
+            a portion of the raw data. Thus, the entire raw signal must be
+            read into memory, and the signal will subsequently be sliced
+            in time if needed. As a result, specifying a small `tlim` will
+            not save computational time but will end up saving memory.
+
+            [tlim] = s
+
+        '''
         self.shot = shot
         self.channel = channel
 
