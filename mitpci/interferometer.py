b94432885a3adb780b7e44b9e6e9af2ec1020286
emd <emd@mit.edu>
2016-06-27 22:26:35 -0700

    "Secular change" identification and corresponding testing.

--- mitpci/interferometer.py
+++ mitpci/interferometer.py
@@ -17,6 +17,9 @@ class Demodulated(object):
 
     Attributes:
     -----------
+    shot - int
+        The shot number of the retrieved signals.
+
     I - :py:class:`Signal <mitpci.signal.Signal>` instance
         The in-phase (I) signal of the interferometer.
 
@@ -67,3 +70,83 @@ class Demodulated(object):
         self.Q = Signal(shot, channel_Q, **signal_kwargs)
 
         return
+
+
+def secular_change_indices(x, secular_change=1, plot=False):
+    '''Get indices between which `x` changes by `secular_change`.
+
+    Parameters:
+    -----------
+    x - array_like, (`N`,)
+        The signal to be examined for secular variation.
+
+    secular_change - float
+        The minimum amount that `x` must change by to be considered
+        a secular variation (as opposed to e.g. a small-amplitude
+        periodic variation)
+
+    plot - bool
+        If True, plot signal `x` and its corresponding windows
+        of secular change. Plotting is not recommended for long signals.
+
+    Returns:
+    --------
+    ind - array_like, (`M`) with `M` < `N`
+        The indices for which `x` changes by `secular_change`;
+        that is, for `i` in {1, 2, ... `M`}, we will have:
+
+            |x[ind[i]] - x[ind[i - 1]]| >= secular_change
+
+        The boundary case for i = 0 satisfies
+
+                |x[ind[0]] - x[0]| >= secular_change
+
+    '''
+    # Convert "analog" signal `x` into a "digital" signal
+    # with minimum bit size `secular_change`
+    xdig = np.floor(x / secular_change) * secular_change
+
+    # Find positions where `xdig` jumps from one "bit" to another,
+    # with `jump` storing the indices of the *first* point
+    # following each jump
+    jump = np.where(np.diff(xdig) != 0)[0] + 1
+
+    # Handle the (rare) case that `jump[-1]` exceeds the bounds
+    # of the signal
+    if jump[-1] == len(xdig):
+        jump = jump[:-1]
+
+    # To be a *secular* jump (rather than a jump corresponding
+    # to e.g. a small-amplitude periodic signal), the value of
+    # the "digital" signal at `jump[i]` must *not* be equal to
+    # its value immediately prior to `jump[i - 1]`. (Sketching
+    # the  "analog" and "digital" signals may aid in the
+    # understanding of this point).
+    secular_jump = np.where(
+        xdig[jump[1:]] != xdig[jump[:-1] - 1])[0]
+
+    # The secular nature of any given jump is dependent
+    # on the signal value immediately prior to the preceding jump.
+    # Now, the *first* jump does *not* have a preceding jump, and
+    # we can therefore not determine whether or not it is
+    # a secular jump. This explains the `[1:]` indexing below.
+    ind = jump[1:][secular_jump]
+
+    if plot:
+        plt.figure()
+
+        plt.plot(x)
+        plt.plot(xdig)
+        plt.plot(jump, xdig[jump], 'ro')
+        for i in ind:
+            plt.axvline(i, c='k')
+
+        plt.xlabel('index')
+        plt.ylabel('value')
+        plt.legend(
+            ['signal', '"digital" signal', '"jumps"', 'secular changes'],
+            loc='upper right')
+
+        plt.show()
+
+    return ind
