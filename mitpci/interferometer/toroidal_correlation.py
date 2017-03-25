be101dc17a1151191f3d41245f8741919760ceb8
emd <emd@mit.edu>
2017-03-25 14:58:50 -0700

    Toroidal correlation; input checking; automated tests, pt. 2

---
+++ mitpci/interferometer/toroidal_correlation.py
@@ -0,0 +1,43 @@
+'''This module implements a class for correlating the MIT heterodyne
+interferometer (toroidal location: 285 degrees, R = 1.98 m) with the
+DIII-D V2 heterodyne interferometer (toroidal location: 240 degrees,
+R = 1.94 m).
+
+'''
+
+
+# Standard library imports
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# Related 3rd-party imports
+from .demodulated import Demodulated
+import bci
+from random_data.spectra import CrossSpectralDensity
+
+
+class ToroidalCorrelation(CrossSpectralDensity):
+    def __init__(self, D, V2=None, trigger_offset=None):
+        self._checkInputs(D, V2)
+
+    def _checkInputs(self, D, V2):
+        # Valid types for `D` and `V2`
+        Dtype = Demodulated
+        V2type = bci.signal.Signal
+
+        # Check that user-provided `D` is of correct type
+        if type(D) is not Dtype:
+            raise ValueError('`D` must be of type %s' % Dtype)
+
+        # Check user-provided `V2` is of correct type and
+        # is compatible with `D`
+        if V2 is None:
+            pass
+        elif type(V2) is not V2type:
+            raise ValueError('`V2` must be of type %s' % V2type)
+        elif D.shot != V2.shot:
+            raise ValueError('`D` and `V2` must correspond to same shot')
+        elif (D.t()[0] > V2.t()[-1]) or (D.t()[-1] < V2.t()[0]):
+            raise ValueError('No temporal overlap between `D` and `V2`')
+
+        return
