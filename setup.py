e79fac65c3db687cf64c4644ba055426817f5e4e
emd <emd@mit.edu>
2017-03-27 19:44:07 -0700

    "Updated" dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ config = {
     'name': 'mitpci',
     'version': '0.3',
     'packages': ['mitpci'],
-    'install_requires': ['numpy', 'matplotlib', 'MDSplus', 'nose'],
+    'install_requires': [
+        'numpy', 'matplotlib', 'scipy', 'MDSplus', 'nose'],
+        # 'random_data', 'bci', 'magnetics'],  # <- Not on PyPI
     'author': 'Evan M. Davis',
     'author_email': 'emd@mit.edu',
     'url': '',
