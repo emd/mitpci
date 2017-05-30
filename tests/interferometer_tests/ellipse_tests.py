26b6a1281b6536a1e021cfabdaaa1a7a19907fbd
emd <emd@mit.edu>
2017-05-30 15:32:55 -0700

    Automated testing of ellipse fitting

--- tests/interferometer_tests/ellipse_tests.py
+++ tests/interferometer_tests/ellipse_tests.py
@@ -70,3 +70,56 @@ def test_FittedEllipse_getSlice():
         slice(360, None))
 
     return
+
+
+def test_FittedEllipse_getFits():
+    E = np.arange(0, 2 * np.pi, np.pi / 180)
+
+    # Single ellipse
+    # Note: Not quite sure on angle convention
+    # of ellipse-fitting algorithm, so just use
+    # small, positive value (which ellipse-fitting
+    # algorithm maps back onto the same value)
+    axes1 = np.abs(np.random.randn(2))
+    a1 = np.max(axes1)
+    b1 = np.min(axes1)
+    x01 = np.random.randn()
+    y01 = np.random.randn()
+    theta1 = 0.1
+    x1, y1 = _ellipse(a1, b1, x01, y01, theta1, E=E)
+
+    FE = FittedEllipse(x1, y1, [0])
+
+    np.testing.assert_allclose(FE.a, np.array([a1]))
+    np.testing.assert_allclose(FE.b, np.array([b1]))
+    np.testing.assert_allclose(FE.x0, np.array([x01]))
+    np.testing.assert_allclose(FE.y0, np.array([y01]))
+    np.testing.assert_allclose(
+        FE.theta,
+        np.array([theta1]))
+
+    # Generate 2nd, distinct ellipse
+    # Note: Not quite sure on angle convention
+    # of ellipse-fitting algorithm, so just use
+    # small, positive value (which ellipse-fitting
+    # algorithm maps back onto the same value)
+    axes2 = np.abs(np.random.randn(2))
+    a2 = np.max(axes2)
+    b2 = np.min(axes2)
+    x02 = np.random.randn()
+    y02 = np.random.randn()
+    theta2 = 0.2
+    x2, y2 = _ellipse(a2, b2, x02, y02, theta2, E=E)
+
+    FE = FittedEllipse(
+        np.concatenate((x1, x2)),
+        np.concatenate((y1, y2)),
+        [0, len(E)])
+
+    np.testing.assert_allclose(FE.a, np.array([a1, a2]))
+    np.testing.assert_allclose(FE.b, np.array([b1, b2]))
+    np.testing.assert_allclose(FE.x0, np.array([x01, x02]))
+    np.testing.assert_allclose(FE.y0, np.array([y01, y02]))
+    np.testing.assert_allclose(FE.theta, np.array([theta1, theta2]))
+
+    return
