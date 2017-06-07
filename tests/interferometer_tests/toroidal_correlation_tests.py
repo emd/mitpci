4f373997b34afe58f27ac989e8935195c587e212
emd <emd@mit.edu>
2017-06-07 08:34:20 -0700

    Updated toroidal correlation to use `Phase`; updated testing

--- tests/interferometer_tests/toroidal_correlation_tests.py
+++ tests/interferometer_tests/toroidal_correlation_tests.py
@@ -1,6 +1,6 @@
 import numpy as np
 from nose import tools
-from mitpci.interferometer import Demodulated, ToroidalCorrelation
+from mitpci.interferometer import Lissajous, Phase, ToroidalCorrelation
 import bci
 
 
@@ -10,28 +10,29 @@ def test__checkSignals():
     dt = tlim[1] - tlim[0]
 
     # Load data
-    D = Demodulated(shot, tlim=tlim)
+    L = Lissajous(shot, tlim=tlim)
+    Ph = Phase(L)
     V2 = bci.signal.Signal(shot, tlim=tlim)
 
-    # Need to pass object of type :py:class:`Demodulated
-    # <mitpci.signal.demodulated.Demodulated>`, not just signal
-    tools.assert_raises(ValueError, ToroidalCorrelation, D.getPhase())
+    # Need to pass object of type :py:class:`Phase
+    # <mitpci.signal.demodulated.Phase>`, not just signal
+    tools.assert_raises(ValueError, ToroidalCorrelation, Ph.x)
 
     # Need to pass :py:class:`Signal <bci.signal.Signal>` for V2 data
-    tools.assert_raises(ValueError, ToroidalCorrelation, D, {'V2': V2.x})
+    tools.assert_raises(ValueError, ToroidalCorrelation, Ph, {'V2': V2.x})
 
-    # `D` and `V2` must correspond to same shot
+    # `Ph` and `V2` must correspond to same shot
     V2.shot += 1
-    tools.assert_raises(ValueError, ToroidalCorrelation, D, {'V2': V2})
+    tools.assert_raises(ValueError, ToroidalCorrelation, Ph, {'V2': V2})
 
-    # `V2` temporal range just below that of `D`
+    # `V2` temporal range just below that of `Ph`
     tlim_lo = tlim - dt - (1. / V2.Fs)
     V2 = bci.signal.Signal(shot, tlim=tlim_lo)
-    tools.assert_raises(ValueError, ToroidalCorrelation, D, {'V2': V2})
+    tools.assert_raises(ValueError, ToroidalCorrelation, Ph, {'V2': V2})
 
-    # `V2` temporal range just above that of `D`
+    # `V2` temporal range just above that of `Ph`
     tlim_hi = tlim + dt + (1. / V2.Fs)
     V2 = bci.signal.Signal(shot, tlim=tlim_hi)
-    tools.assert_raises(ValueError, ToroidalCorrelation, D, {'V2': V2})
+    tools.assert_raises(ValueError, ToroidalCorrelation, Ph, {'V2': V2})
 
     return
