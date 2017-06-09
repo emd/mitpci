14b22de1cb358430abb072f7b042d9c0605fd05d
emd <emd@mit.edu>
2017-06-09 12:00:18 -0700

    Bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 config = {
     'name': 'mitpci',
-    'version': '0.3',
+    'version': '0.4',
     'packages': ['mitpci'],
     'install_requires': [
         'numpy', 'matplotlib', 'scipy', 'MDSplus', 'nose'],
